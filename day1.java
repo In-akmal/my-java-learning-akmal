b86eb8781bba5477e92e70f3b393c09427bec772
Ayyar Akmal <akmalayyar9@gmail.com>
2025-08-23 15:51:59 -0400

    day 1

---
+++ day1.java
@@ -0,0 +1,10 @@
+class day1 {
+day1(){
+}
+    public static void main(String[] args){
+    int a = 15;
+    int b = 5;
+    int c = a+b;
+    System.out.println("the sum of two munber is "+c);
+    }
+}
