326d42279b51f535b6ecd7131024aca671bf63ab
Ayyar Akmal <akmalayyar9@gmail.com>
2026-08-18 13:57:52 -0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,22 @@
+
+class Solution {
+     static void rotateArr(int arr[], int d) {
+         int n = arr.length;
+         Queue<Integer> q = new LinkedList<>();
+         d = d%n;
+
+         for(int i=d;i<n;i++){
+             q.add(arr[i]);
+         }
+
+         for(int i=0;i<d;i++){
+             q.add(arr[i]);
+         }
+
+         for(int i = 0; i < n; i++){
+             int removeEle = q.remove();
+             arr[i] = removeEle;
+         }
+     }
+ }
+
