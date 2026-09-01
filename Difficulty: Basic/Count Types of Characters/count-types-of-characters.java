4fffc66fec598f7091d14a27443def5d65445718
Ayyar Akmal <akmalayyar9@gmail.com>
2026-09-01 10:46:08 -0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Types of Characters/count-types-of-characters.java
@@ -0,0 +1,18 @@
+class Sol {
+    int[] count(String s) {
+        // your code here
+        int[] res=new int[4];
+        for(char ch: s.toCharArray()) {
+            if(Character.isUpperCase(ch)) {
+                res[0]++;
+            } else if (Character.isLowerCase(ch)) {
+                res[1]++;
+            } else if (Character.isDigit(ch)) {
+                res[2]++;
+            } else {
+                res[3]++;
+            }
+        }
+        return res;
+    }
+}
